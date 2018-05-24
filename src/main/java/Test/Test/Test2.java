538fcbbb4244b53b6d154a5b39296a46e63d8e48
pavank30 <pavan.krishna30@gmail.com>
2018-05-24 17:49:08 +0530

    updated Test2

--- src/main/java/Test/Test/Test2.java
+++ src/main/java/Test/Test/Test2.java
@@ -4,7 +4,10 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("logout");
+		System.out.println("testing");
 		System.out.println("git local changes");
+		System.out.println("git pull");
+		
 	}
 
 }
